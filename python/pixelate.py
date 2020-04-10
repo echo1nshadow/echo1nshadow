b71c93151857c5630019fc93566884a217f943fb
echo <echo1nshadow@qq.com>
2020-04-10 16:26:30 +0800

    添加通用接口
    date:2020/4/10
    by echo1nshadow@qq.com

--- python/pixelate.py
+++ python/pixelate.py
@@ -7,16 +7,16 @@ import sys
 import getopt
 
 def open_pic(argv):
-    im=Image.open(argv);
+    im=Image.open(argv)
     return im
 
 def load_pic(im):
     return numpy.array(im)
 
 def print_pic(im):
-    arr=load_pic(im);
-    i = 0;
-    j = 0;
+    arr=load_pic(im)
+    i = 0
+    j = 0
     while( i < im.height):
         j = 0
         while( j < im.width):
@@ -24,75 +24,97 @@ def print_pic(im):
             j+=1
         i += 1
 
-def color_pic(arr,x,y,block,color):
-    i = x;
-    j = y;
-    while( i < x+block ):
-        j = y;
-        while( j < y+block ):
-            arr[i,j] = color;
+def color_pic(arr,x,y,value,color):
+    i = x
+    j = y
+    while( i < x+value ):
+        j = y
+        while( j < y+value ):
+            arr[i,j] = color
             j += 1
         i += 1
     return arr
 
-def get_average_color(arr,x,y,block):
-    r = g = b = 0;
-    i = x;
-    j = y;
-    while( i < x+block ):
-        j = y;
-        while( j < y+block ):
+def get_average_color(arr,x,y,value):
+    r = g = b = 0
+    i = x
+    j = y
+    while( i < x+value ):
+        j = y
+        while( j < y+value ):
             r += arr[i,j][0]
             g += arr[i,j][1]
             b += arr[i,j][2]
             j += 1
         i += 1
-    r = round(r / ( block * block))
-    g = round(g / ( block * block))
-    b = round(b / ( block * block))
+    r = round(r / ( value * value))
+    g = round(g / ( value * value))
+    b = round(b / ( value * value))
     return tuple((r,g,b))
+
+
+def pixelated_pic(im):
+    global value
+    arr=load_pic(im)
+    i = 0
+    j = 0
+    while( i < im.height ):
+        j = 0
+        while( j < im.width ):
+            color_tuple = get_average_color(arr,i,j,value)
+            new_arr = color_pic(arr,i,j,value,color_tuple)
+            j += value
+        i += value
+    return new_arr
+
+def lighten_pic(im):
+    arr = load_pic(im)
+    i = 0
+    j = 0
+    while ( i < im.height ):
+        j = 0
+        while( j < im.width ):
+            arr[i,j] = (arr[i,j][0]+10, arr[i,j][1]+10, arr[i,j][2]+10)
+            j += 1
+        i += 1
+    return arr
 
 def save_pic(arr,dest_name):
     Image.fromarray(arr).save(dest_name)
 
-def pixelated_pic(im,block):
-    arr=load_pic(im);
-    i = 0;
-    j = 0;
-    while( i < im.height ):
-        j = 0;
-        while( j < im.width ):
-            color_tuple = get_average_color(arr,i,j,block)
-            new_arr = color_pic(arr,i,j,block,color_tuple)
-            j += block
-        i += block
-    return new_arr
-
+def process_pic(im,proc):
+    return proc(im)
 
 def main(argv):
-    block = 1
+    global value
     src_name = ""
     dest_name = ""
     try:
-        opts,args = getopt.getopt(argv,"b:i:o:",["block="])
+        opts,args = getopt.getopt(argv,"plv:i:o:",["value="])
     except getopt.GetoptError:
         print("parse argv error")
     for opt,arg in opts:
-        if opt == '-b':
-            block = int(arg)
-            print("block:%d"%block)
+        if opt == '-v':
+            value = int(arg)
+            print("value:%d"%value)
         elif opt == '-o':
             dest_name = arg
             print(dest_name)
+        elif opt == '-l':
+            proc = lighten_pic
+        elif opt == '-p':
+            proc = pixelated_pic
         elif opt == '-i':
             src_name = arg
             print(src_name)
 
     im=open_pic(src_name)
     arr = load_pic(im)
-    new_arr = pixelated_pic(im,block)
+    new_arr = process_pic(im,proc)
     save_pic(new_arr,dest_name)
 
+
+value = 1
 
 if __name__=="__main__":
     main(sys.argv[1:])
